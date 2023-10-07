f8451f831a3359a50b480a1da0c9c2b08dda78cb
TommyPhi <tommy.phi04@gmail.com>
2023-10-07 17:23:19 -0400

    Completed each content pages including JavaScript examples. Also added new example images for each content page

---
+++ script.js
@@ -0,0 +1,31 @@
+// for loop example function
+let forLoop = () => {
+    let array=['a', 'b', 'c', 'd']
+    for (let i=0; i<array.length; i++) {
+        console.log(array[i])
+    }
+}
+
+// while loop example function
+let whileLoop = () => {
+    let x = 0;
+    while (x<=10) {
+        console.log(`${x} chickens crossed the road`);
+        x++;
+    }
+}
+
+// do...while loop example function
+let doWhileLoop = () => {
+    let y = 0;
+    do {
+        y++;
+        console.log(`You leveled up! You are now level: ${y}`)
+    }
+    while (y < 10);
+}
+
+// Build your own loop under here
+let byol = () => {
+    
+}
