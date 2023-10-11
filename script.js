fe9602d518524d943976e2735bd3f2eedaaf2355
TommyPhi <tommy.phi04@gmail.com>
2023-10-11 09:46:41 -0400

    Added new content pages for other for loops

--- script.js
+++ script.js
@@ -6,6 +6,23 @@ let forLoop = () => {
     }
 }
 
+//for in loop example function
+const object = { a: 1, b: 2, c: 3 };
+let forInLoop = () => {
+    for (const property in object) {
+        console.log(`${property}: ${object[property]}`);
+      }
+}
+
+//for of loop example function
+const array1 = ['a', 'b', 'c'];
+let forOfLoop = () => {
+    for (const element of array1) {
+        console.log(element);
+      }
+}
+
+
 // Build your own loop under here
 let byol = () => {
     
